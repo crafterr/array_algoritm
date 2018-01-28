01834d9ac234ac4ba52280cd888f0eae075f9e0d
crafter <adam@crafter.net.pl>
2018-01-28 20:18:45 +0100

    insert first

--- lists/list1.php
+++ lists/list1.php
@@ -4,9 +4,15 @@ require ABSPATH.'vendor/autoload.php';
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
+
+/**
+ * Class Node
+ * Represetant one node
+ */
 class Node
 {
     public $data = NULL;
+
     public $next = NULL;
 
     public function __construct(string $data = NULL)
@@ -15,14 +21,17 @@ class Node
     }
 }
 
-
 class LinkedList
 {
     private $_firstNode = NULL;
+
     private $_totalNode = 0;
 
-
-
+    /**
+     * Insert string to node
+     * @param string|NULL $data
+     * @return true
+     */
     public function insert(string $data = NULL)
     {
         $newNode = new Node($data);
@@ -34,25 +43,42 @@ class LinkedList
         else
         {
             $currentNode = $this->_firstNode;
-
-            while ($currentNode->next !== NULL)
-            {
+            while ($currentNode->next !== NULL) {
                 $currentNode = $currentNode->next;
             }
             $currentNode->next = $newNode;
         }
         $this->_totalNode++;
-        return TRUE;
+        return true;
+
     }
 
-    public function totalNodes()
+    /**
+     * Inserting at the first node
+     * @param string|NULL $data
+     * @return true
+     */
+    public function insertAtFirst(string $data = NULL)
     {
-        return $this->_totalNode;
+        $newNode = new Node($data);
+
+        if ($this->_firstNode === NULL)
+        {
+            $this->_firstNode = &$newNode;
+        }
+        else
+        {
+            $currentFirstNode = $this->_firstNode;
+            $this->_firstNode = &$newNode;
+            $newNode->next = $currentFirstNode;
+        }
+        $this->_totalNode++;
+        return true;
     }
 
     public function display()
     {
-        echo "Total book titles: ".$this->totalNodes().'<br />';
+        echo "Total book titles: ".$this->_totalNode.'<br />';
         $currentNode  = $this->_firstNode;
         while ($currentNode !== NULL)
         {
@@ -60,13 +86,14 @@ class LinkedList
             $currentNode = $currentNode->next;
         }
     }
-
-
 }
+
 
 $BookTitles = new LinkedList();
 $BookTitles->insert("Introduction to Algorithm");
 $BookTitles->insert("Introduction to PHP and Data structures");
 $BookTitles->insert("Programming Intelligence");
+$BookTitles->insertAtFirst("First introduction to Algoritm");
+$BookTitles->insert("Adam ma kota");
 $BookTitles->display();
 dump($BookTitles);
